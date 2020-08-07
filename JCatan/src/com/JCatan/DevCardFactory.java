02e02081b1db1042877144892f5a3152d4ecd4fc
Jared <jaredlham@gmail.com>
2020-08-07 10:21:17 -0600

    Implemented Bank class and dev card factory

--- JCatan/src/com/JCatan/DevCardFactory.java
+++ JCatan/src/com/JCatan/DevCardFactory.java
@@ -1,14 +1,46 @@
 package com.JCatan;
 
+import java.util.ArrayList;
+import java.util.Collections;
 import java.util.List;
+import java.util.Stack;
 
 public class DevCardFactory {
+    private static final int NUM_KNIGHT_CARDS = 14;
+    private static final int NUM_MONOPOLY_CARDS = 2;
+    private static final int NUM_ROAD_BUILDING_CARDS = 2;
+    private static final int NUM_YEAR_OF_PLENTY_CARDS = 2;
+    private static final int NUM_VICTORY_POINT_CARDS = 5;
 	
 	/**
 	 * @return
 	 */
-	public static List<DevelopmentCard> makeDevCards(){
-		return null;
+	public static Stack<DevelopmentCard> makeDevCards(){
+	    Stack<DevelopmentCard> devCards = new Stack<>();
+	    
+	    for (int i = 0 ; i < NUM_KNIGHT_CARDS; i++) {
+	        devCards.add(new KnightDevelopmentCard());
+	    }
+	    
+	    for (int i = 0 ; i < NUM_MONOPOLY_CARDS; i++) {
+            devCards.add(new MonopolyDevelopmentCard());
+        }
+	    
+	    for (int i = 0 ; i < NUM_ROAD_BUILDING_CARDS; i++) {
+            devCards.add(new RoadBuildingDevelopmentCard());
+        }
+	    
+	    for (int i = 0 ; i < NUM_YEAR_OF_PLENTY_CARDS; i++) {
+            devCards.add(new YearOfPlentyDevelopmentCard());
+        }
+	    
+	    for (int i = 0 ; i < NUM_VICTORY_POINT_CARDS; i++) {
+            devCards.add(new VictoryPointDevelopmentCard());
+        }
+	    
+	    Collections.shuffle(devCards);
+	    
+		return devCards;
 	}
 
 }
