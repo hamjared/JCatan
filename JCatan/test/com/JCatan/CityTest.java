cb5e7aceed81891b10562242da3d0214a596e1c6
Jared <jaredlham@gmail.com>
2020-08-17 07:43:34 -0600

    fixed

--- JCatan/test/com/JCatan/CityTest.java
+++ JCatan/test/com/JCatan/CityTest.java
@@ -17,7 +17,7 @@ public class CityTest extends TestCase
 
     public void testGetCost()
     {
-        Building building = new City(new HumanPlayer("Test"));
+        Building building = new City(new HumanPlayer("Test"), null);
 
         assertEquals(Integer.valueOf(2),
                 building.getCost().get(ResourceType.WHEAT));
