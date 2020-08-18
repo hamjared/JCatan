ef6e1698ba3c111287203b90b987300608738a48
Jared <jaredlham@gmail.com>
2020-08-18 10:42:02 -0600

    implemeted performAction

--- JCatan/src/com/JCatan/KnightDevelopmentCard.java
+++ JCatan/src/com/JCatan/KnightDevelopmentCard.java
@@ -3,9 +3,13 @@ package com.JCatan;
 public class KnightDevelopmentCard extends DevelopmentCard {
 
     @Override
-    public void performAction() throws InvalidDevCardUseException
+    public void performAction(DevCardAction devCardAction) throws InvalidDevCardUseException
     {
-        // TODO DevelopmentCard: performAction
+    	int stealCardIndex = (int) Math.round(Math.random() * devCardAction.getStealFromPlayer().getResources().size());
+        ResourceCard stolenCard = devCardAction.getStealFromPlayer().getResources().get(stealCardIndex);
+        devCardAction.getStealFromPlayer().getResources().remove(stealCardIndex);
+        
+        devCardAction.getCurPlayer().getResources().add(stolenCard);
         
     }
     
--- JCatan/src/com/JCatan/YearOfPlentyDevelopmentCard.java
+++ JCatan/src/com/JCatan/YearOfPlentyDevelopmentCard.java
@@ -2,16 +2,28 @@ package com.JCatan;
 
 public class YearOfPlentyDevelopmentCard extends DevelopmentCard {
 
-    @Override
-    public void performAction() throws InvalidDevCardUseException
-    {
-        // TODO DevelopmentCard: performAction
-        
-    }
+
     
     public String toString() {
     	return "Year of Plenty";
     }
+
+	@Override
+	public void performAction(DevCardAction devCardAction) throws InvalidDevCardUseException {
+		Bank bank = devCardAction.getBank();
+		ResourceType rt = devCardAction.getStealResourceType();
+		Player curPlayer = devCardAction.getCurPlayer();
+		
+		try {
+			curPlayer.getResources().add(bank.takeResourceCard(rt));
+			curPlayer.getResources().add(bank.takeResourceCard(rt));
+		} catch (InsufficientResourceCardException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		
+		
+	}
 
 
 
