b4404faf2b8dcf12461b6c14d1c759ffeee2324a
Jared <jaredlham@gmail.com>
2020-08-19 12:43:33 -0600

    Took out action from KnightDevelopment card. It is handled by the robber

--- JCatan/src/com/JCatan/KnightDevelopmentCard.java
+++ JCatan/src/com/JCatan/KnightDevelopmentCard.java
@@ -5,11 +5,11 @@ public class KnightDevelopmentCard extends DevelopmentCard {
     @Override
     public void performAction(DevCardAction devCardAction) throws InvalidDevCardUseException
     {
-    	int stealCardIndex = (int) Math.round(Math.random() * devCardAction.getStealFromPlayer().getResources().size());
-        ResourceCard stolenCard = devCardAction.getStealFromPlayer().getResources().get(stealCardIndex);
-        devCardAction.getStealFromPlayer().getResources().remove(stealCardIndex);
-        
-        devCardAction.getCurPlayer().getResources().add(stolenCard);
+//    	int stealCardIndex = (int) Math.round(Math.random() * devCardAction.getStealFromPlayer().getResources().size());
+//        ResourceCard stolenCard = devCardAction.getStealFromPlayer().getResources().get(stealCardIndex);
+//        devCardAction.getStealFromPlayer().getResources().remove(stealCardIndex);
+//        
+//        devCardAction.getCurPlayer().getResources().add(stolenCard);
         
     }
     
